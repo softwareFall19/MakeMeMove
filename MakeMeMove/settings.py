c4d6297fbafef206acaee712917042a117a3d5b4
softwareFall19 <softwareFall19@gmail.com>
2019-12-02 04:42:10 -0500

    django-heroku

--- MakeMeMove/settings.py
+++ MakeMeMove/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -155,3 +156,4 @@ EMAIL_HOST_USER = 'softwarefall19'
 EMAIL_HOST_PASSWORD = 'Fallsemester19'
 EMAIL_PORT = 587
 
+django_heroku.settings(locals())
