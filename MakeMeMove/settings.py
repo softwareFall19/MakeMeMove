cf2f2fca5fb3e34c949a7ef1b3e55b189adc46b2
softwareFall19 <softwareFall19@gmail.com>
2019-12-03 08:44:33 -0500

    deploy4

--- MakeMeMove/settings.py
+++ MakeMeMove/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'm4pi1(j3j3p_8p8z9p5^ee3lep#t)7fmipoxspf=q5z&1&+o3#'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
