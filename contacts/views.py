a83809cffec2da21d94908e93fce886c0e224cd5
softwareFall19 <softwareFall19@gmail.com>
2019-12-04 19:30:32 -0500

    rental confirmation email

--- contacts/views.py
+++ contacts/views.py
@@ -50,10 +50,27 @@ def infoRental(request):
         rental_id = request.POST.get('rental_id')
         information = request.POST.get('information')
         user_id = request.POST.get('user_id')
+        agent_email = request.POST.get('agent_email')
 
         contact_by_email = ContactRental(name=name, email=email, phone=phone, rental_id=rental_id, information=information, user_id=user_id)
 
         contact_by_email.save()
+
+        send_mail(
+            'Requesting Home Information',
+            'A request for this rental ' + rental_id,
+            settings.EMAIL_HOST_USER,
+            [agent_email],
+            fail_silently=False
+
+        )
+        send_mail(
+            'Thank you for contacting MakeMeMove',
+            'Thank you for contacting MakeMeMove, You will soon be contacted by an agent',
+            settings.EMAIL_HOST_USER,
+            [contact_by_email.email],
+            fail_silently=False   
+        )
 
         messages.success(request, "Thank you for your request, we will contact you shortly")
         return redirect('/renters/'+rental_id)
