607f0a193ca3d338f73b55c7425c40437d678684
softwareFall19 <softwareFall19@gmail.com>
2019-12-04 20:41:00 -0500

    secret admin user and pass

--- MakeMeMove/settings.py
+++ MakeMeMove/settings.py
@@ -154,8 +154,8 @@ STRIPE_PUBLISHABLE_KEY = 'pk_test_wFwG5kndLk5CnhGyhLHDcVzr00zHEd12rd'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'softwarefall19'
-EMAIL_HOST_PASSWORD = 'Fallsemester19'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 
 django_heroku.settings(locals())
